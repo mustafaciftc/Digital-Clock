64ba4bf9f616edb540a478c48c43ce95535c1bf2
Mustafa Çiftci <123982648+mustafaciftc@users.noreply.github.com>
2023-05-29 09:56:26 +0300

    script.js

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -4,7 +4,7 @@ function showTime() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    am_pm = "PM";
+    am_pm = "AM";
 
     if (hour > 12) {
         hour -= 12;
